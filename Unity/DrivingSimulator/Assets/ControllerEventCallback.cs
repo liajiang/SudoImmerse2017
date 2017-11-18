a729db1e73e00640fa13a1a469f2b590291e2ed8
yl573 <yl573@cam.ac.uk>
2017-11-18 14:48:02 +0000

    added interactive objects

---
+++ Unity/DrivingSimulator/Assets/ControllerEventCallback.cs
@@ -0,0 +1,44 @@
+﻿namespace Backend
+{
+	using UnityEngine;
+	using VRTK;
+
+	public class ControllerEventCallbacks : MonoBehaviour
+	{
+		private VRTK_ControllerEvents events;
+
+		private void Start()
+		{
+			events = GetComponent<VRTK_ControllerEvents> ();
+			if (events == null)
+			{
+				Debug.Log("Cannot get VRTK controller events");
+				return;
+			}
+				
+			events.TouchpadPressed += new ControllerInteractionEventHandler(DoTouchpadPressed);
+			events.TouchpadReleased += new ControllerInteractionEventHandler(DoTouchpadReleased);
+
+			events.TriggerAxisChanged += new ControllerInteractionEventHandler(DoTouchpadAxisChanged);
+
+
+		}
+			
+		private void DoTouchpadPressed(object sender, ControllerInteractionEventArgs e)
+		{
+			// emit grab
+		}
+
+		private void DoTouchpadReleased(object sender, ControllerInteractionEventArgs e)
+		{
+			// emit ungrab
+		}
+			
+		private void DoTouchpadAxisChanged(object sender, ControllerInteractionEventArgs e)
+		{
+			// emit throttle/break
+		}
+
+
+	}
+}
