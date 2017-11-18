331c63e3e7e32bc2f241c5e272b7d13ae1360979
frozenL <liangcissy@gmail.com>
2017-11-18 18:21:13 +0000

    handled collision

---
+++ Unity/DrivingSimulator/Assets/rigiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigiController : MonoBehaviour {
+
+	public float speed = 5;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		Debug.Log ("CCCCCollision!");
+	}
+
+	void FixedUpdate ()
+	{
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * 15);	
+	}
+}
