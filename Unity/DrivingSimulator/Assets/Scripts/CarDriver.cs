a45b40aeffb7a958b8d234de2fded6af4b1cba49
Dao Zhou <zhoudada123@hotmail.com>
2017-11-18 19:36:02 +0000

    Implemented car driver.

---
+++ Unity/DrivingSimulator/Assets/Scripts/CarDriver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDriver : MonoBehaviour {
+    public event EventHandler<DirectionChangedEventArgs> DirectionChanged;
+
+	// Use this for initialization
+	void Awake () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unity/DrivingSimulator/Assets/Scripts/DirectionChangedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DirectionChangedEventArgs : EventArgs {
+    public float Direction { get; set; }
+    public DirectionChangedEventArgs(int direction)
+    {
+        Direction = direction;
+    }
+}
