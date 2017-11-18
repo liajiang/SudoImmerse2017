d4852bf079e6f11821247299b789a0ac82b7daa0
yl573 <yl573@cam.ac.uk>
2017-11-18 19:46:28 +0000

    added wheel rotation scripts

---
+++ Unity/DrivingSimulator/Assets/Scripts/WheelRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelRotation : MonoBehaviour {
+
+	public float rotationMultiplier;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		float forwardVelocity = rb.velocity.z;
+		transform.Rotate(Vector3.right * Time.deltaTime * rotationMultiplier * forwardVelocity);
+	}
+}
