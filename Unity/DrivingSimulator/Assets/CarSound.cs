c57ab26358f535f26a871567a41b01cfb7493403
frozenL <liangcissy@gmail.com>
2017-11-19 13:51:21 +0000

    changed carInteriorController

--- Unity/DrivingSimulator/Assets/CarSound.cs
+++ Unity/DrivingSimulator/Assets/CarSound.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class CarSound : MonoBehaviour {
 
-	public CarDriver CarDriver;
+	public CarController CarDriver;
 
 	private AudioSource audio;
 
 	void Start()
 	{
 		audio = GetComponent<AudioSource>();
-		CarDriver.AccelerationChanged += OnAccelerationChanged;
+//		CarDriver.AccelerationChanged += OnAccelerationChanged;
 	}
 		
 	void OnAccelerationChanged(object sender, AccelerationChagnedEventArgs args)
 	{
 		float vol = args.Acceleration;
-		if (vol < 0.1) {
-			vol = 0.1;
+		if (vol < 0.1f) {
+			vol = 0.1f;
 		}
 		audio.volume = vol;
 	}
