c7b77a23241a1b285fb79043614641895d35876e
frozenL <liangcissy@gmail.com>
2017-11-19 12:17:08 +0000

    envs and simple controls

---
+++ Unity/DrivingSimulator/Assets/CarInteriorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarInteriorController : MonoBehaviour {
+
+	public Transform Target;
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Target.position;
+		transform.rotation = Target.rotation;
+	}
+}
