c22f93f8cbfa66d12e87d2d7d54193ffaeb05f1d
zhoudada <zhoudada123@hotmail.com>
2017-11-19 11:01:42 +0000

    Added acceleration mechanism.

--- Unity/DrivingSimulator/Assets/Scripts/AccelerationChagnedEventArgs.cs
+++ Unity/DrivingSimulator/Assets/Scripts/AccelerationChagnedEventArgs.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AccelerationChagnedEventArgs : MonoBehaviour {
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class AccelerationChagnedEventArgs : EventArgs {
+    public float Acceleration { get; set; }
+
+    public AccelerationChagnedEventArgs(float acceleration)
+    {
+        Acceleration = acceleration;
+    }
 }
--- Unity/DrivingSimulator/Assets/Scripts/CarDriver.cs
+++ Unity/DrivingSimulator/Assets/Scripts/CarDriver.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
-public class CarDriver : MonoBehaviour {
+public class CarDriver : MonoBehaviour
+{
+    public VRTK_ControllerEvents LeftController;
+    public VRTK_ControllerEvents RightController;
+
     public event EventHandler<DirectionChangedEventArgs> DirectionChanged;
     public event EventHandler<GearChangedEventArgs> GearChanged;
+    public event EventHandler<AccelerationChagnedEventArgs> AccelerationChanged;
     
     public bool IsReverse { get { return _isReverse; } }
     public float WheelDirection { get { return _wheelDirection; } }
@@ -14,12 +20,25 @@ public class CarDriver : MonoBehaviour {
     private float _wheelDirection;
     private bool _isReverse;
     private float _acceleration;
+    private float _brake;
+    private float _netAcceleration;
+    private float _lastNetAcceleration;
 
     private void Awake()
     {
+        LeftController.TriggerPressed += Accelerate;
+        RightController.TriggerPressed += Brake;
     }
 
-    void Update () {
+    void Update ()
+    {
+        _lastNetAcceleration = _netAcceleration;
+        _netAcceleration = _acceleration - _brake;
+        if (_lastNetAcceleration != _netAcceleration)
+        {
+            var args = new AccelerationChagnedEventArgs(_netAcceleration);
+            OnAccelerationChanged(this, args);
+        }
     }
 
     public void OnDirectionChanged(object sender, DirectionChangedEventArgs args)
@@ -36,6 +55,26 @@ public class CarDriver : MonoBehaviour {
     {
         _isReverse = args.IsReverse;
         var handler = GearChanged;
+        if (handler != null)
+        {
+            handler(sender, args);
+        }
+    }
+
+    private void Accelerate(object sender, ControllerInteractionEventArgs args)
+    {
+        _acceleration = VRTK_ControllerReference.GetRealIndex(args.controllerReference);
+    }
+
+    private void Brake(object sender, ControllerInteractionEventArgs args)
+    {
+        _brake = VRTK_ControllerReference.GetRealIndex(args.controllerReference);
+    }
+
+    public void OnAccelerationChanged(object sender, AccelerationChagnedEventArgs args)
+    {
+        _acceleration = args.Acceleration;
+        var handler = AccelerationChanged;
         if (handler != null)
         {
             handler(sender, args);
