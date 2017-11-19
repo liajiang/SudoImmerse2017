aee70b1e0c42aba0aff5702d0814601414c81a56
Dao Zhou <zhoudada123@hotmail.com>
2017-11-19 12:22:08 +0000

    Added acceleration and braking mechanism.@

--- Unity/DrivingSimulator/Assets/Scripts/CarDriver.cs
+++ Unity/DrivingSimulator/Assets/Scripts/CarDriver.cs
@@ -6,9 +6,6 @@ using VRTK;
 
 public class CarDriver : MonoBehaviour
 {
-    public VRTK_ControllerEvents LeftController;
-    public VRTK_ControllerEvents RightController;
-
     public event EventHandler<DirectionChangedEventArgs> DirectionChanged;
     public event EventHandler<GearChangedEventArgs> GearChanged;
     public event EventHandler<AccelerationChagnedEventArgs> AccelerationChanged;
@@ -23,16 +20,25 @@ public class CarDriver : MonoBehaviour
     private float _brake;
     private float _netAcceleration;
     private float _lastNetAcceleration;
+    private bool _braking;
+    private bool _accelerating;
 
     private void Awake()
     {
-        LeftController.TriggerPressed += Accelerate;
-        RightController.TriggerPressed += Brake;
     }
 
     void Update ()
     {
         _lastNetAcceleration = _netAcceleration;
+        if (!_braking)
+        {
+            _brake = 0;
+        }
+
+        if (!_accelerating)
+        {
+            _acceleration = 0f;
+        }
         _netAcceleration = _acceleration - _brake;
         if (_lastNetAcceleration != _netAcceleration)
         {
@@ -61,19 +67,38 @@ public class CarDriver : MonoBehaviour
         }
     }
 
-    private void Accelerate(object sender, ControllerInteractionEventArgs args)
+    public void AccelerateStart(object sender, ControllerInteractionEventArgs args)
     {
-        _acceleration = VRTK_ControllerReference.GetRealIndex(args.controllerReference);
+        _accelerating = true;
     }
 
-    private void Brake(object sender, ControllerInteractionEventArgs args)
+    public void AccelerateStop(object sender, ControllerInteractionEventArgs args)
     {
-        _brake = VRTK_ControllerReference.GetRealIndex(args.controllerReference);
+        _accelerating = false;
+    }
+
+    public void BrakeStart(object sender, ControllerInteractionEventArgs args)
+    {
+        _braking = true;
+    }
+
+    public void BrakeStop(object sender, ControllerInteractionEventArgs args)
+    {
+        _braking = false;
+    }
+
+    public void Accelerate(object sender, ControllerInteractionEventArgs args)
+    {
+        _acceleration = args.buttonPressure;
+    }
+
+    public void Brake(object sender, ControllerInteractionEventArgs args)
+    {
+        _brake = args.buttonPressure;
     }
 
     public void OnAccelerationChanged(object sender, AccelerationChagnedEventArgs args)
     {
-        _acceleration = args.Acceleration;
         var handler = AccelerationChanged;
         if (handler != null)
         {
